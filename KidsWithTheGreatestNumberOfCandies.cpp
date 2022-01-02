23a84794b49b30b84c1aa8cf1ddaab5cc48db5ce
Anubhav Rajput <72142278+Anubhav-Raj@users.noreply.github.com>
2022-01-03 00:21:46 +0530

    updated

--- KidsWithTheGreatestNumberOfCandies.cpp
+++ KidsWithTheGreatestNumberOfCandies.cpp
@@ -33,6 +33,42 @@
      {
           cin>>candies[l];
      }
+   
+   
+   
+   //accoding to leet code solution
+   class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+             vector<bool> temp;
+            int n= candies.size();
+         string output[n];
+          for (int i = 0; i < n; i++)
+         {
+                 for (int j = 0; j < n; j++)
+                 {
+                   if(candies[i]+extraCandies < candies[j])
+                   {
+                         output[i]= "False";
+                         break;
+                   } else{
+                            output[i]= "Ture";
+                   }
+                 }
+                        
+         }
+         for(int i=0;i<n;i++){
+              if(output[i]=="False"){
+              temp.push_back(false);
+              } else {             
+               temp.push_back(true);
+              }
+         }
+       
+         return temp;
+            
+    }
+};
       int extraCandies;
       cout<<"Enter the Extra Candies:";
       cin>>extraCandies;
